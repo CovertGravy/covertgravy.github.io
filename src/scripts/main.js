c18dd731d9ace1a47af69706bf19666484941d56
covertgravy <ashish.cgwcn@gmail.com>
2020-09-15 20:02:48 +0530

    favicon, logo-mini

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,9 @@
 import "normalize.css";
 import "../css/style.scss";
+
+document.onreadystatechange = function () {
+  if (document.readyState !== "complete") {
+    console.log("loading...");
+  }
+  console.log(document.readyState);
+};
