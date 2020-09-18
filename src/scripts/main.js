93867e5337dc7309815fac531ff1916ad9ddee40
covertgravy <ashish.cgwcn@gmail.com>
2020-09-18 16:58:58 +0530

    intersection observer

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,8 @@ import "normalize.css";
 import "../css/style.scss";
 
 const loadingElement = document.querySelector("#loading");
+const heroSection = document.querySelector("#hero");
+const navbar = document.querySelector("nav");
 
 document.onreadystatechange = function () {
   if (document.readyState === "complete") {
@@ -10,3 +12,20 @@ document.onreadystatechange = function () {
     }, 1800);
   }
 };
+
+const heroSectionObserver = new IntersectionObserver(
+  function (entries, heroSectionObserver) {
+    entries.forEach((entry) => {
+      if (!entry.isIntersecting) {
+        navbar.classList.add("nav-scrolled");
+      } else {
+        navbar.classList.remove("nav-scrolled");
+      }
+    });
+  },
+  {
+    rootMargin: "-100px 0px 0px 0px",
+  }
+);
+
+heroSectionObserver.observe(heroSection);
