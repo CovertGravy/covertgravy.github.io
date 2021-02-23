c785bd82f7de6243c5e02f6fe9809c8f8a94aad8
covertgravy <ashish.cgwcn@gmail.com>
2021-02-23 20:52:48 +0530

    vanilla tilt

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,22 +1,29 @@
-import "normalize.css";
-import "../css/loco-base.css";
-import "../css/style.scss";
+import 'normalize.css';
+import '../css/loco-base.css';
+import '../css/style.scss';
 
-import topbar from "topbar";
+import topbar from 'topbar';
+import VanillaTilt from 'vanilla-tilt';
 
 topbar.show();
 topbar.config({
-  shadowColor: "rgba(0, 0, 0, .1)",
+  shadowColor: 'rgba(0, 0, 0, .1)',
 });
 
-const loadingElement = document.querySelector("#loading");
-const heroSection = document.querySelector("#hero");
-const header = document.querySelector("header");
+const loadingElement = document.querySelector('#loading');
+const heroSection = document.querySelector('#hero');
+const header = document.querySelector('header');
+
+VanillaTilt.init(document.querySelector('.about-figure'), {
+  max: 15,
+  speed: 200,
+  scale: 1.1,
+});
 
 document.onreadystatechange = function () {
-  if (document.readyState === "complete") {
+  if (document.readyState === 'complete') {
     setTimeout(() => {
-      loadingElement.style.display = "none";
+      loadingElement.style.display = 'none';
       topbar.hide();
     }, 1800);
   }
@@ -26,14 +33,14 @@ const heroSectionObserver = new IntersectionObserver(
   function (entries, heroSectionObserver) {
     entries.forEach((entry) => {
       if (!entry.isIntersecting) {
-        header.classList.add("header-scrolled");
+        header.classList.add('header-scrolled');
       } else {
-        header.classList.remove("header-scrolled");
+        header.classList.remove('header-scrolled');
       }
     });
   },
   {
-    rootMargin: "-100px 0px 0px 0px",
+    rootMargin: '-100px 0px 0px 0px',
   }
 );
 
