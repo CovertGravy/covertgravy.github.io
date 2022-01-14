901791313dc732fdbfa67b6d218fa571c4207249
Ashish Shaji <35291614+CovertGravy@users.noreply.github.com>
2022-01-14 20:49:28 +0530

    tech blur blind take

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -42,19 +42,30 @@ heroSectionObserver.observe(heroSection);
 const tech_stack_svgs = document.querySelectorAll('.tech-stack svg');
 const tech_name_div = document.querySelector('.tech-stack-name')
 tech_stack_svgs.forEach(svg => {
-  svg.addEventListener('mouseenter', showTechName(svg.dataset.techname));
-  svg.addEventListener('mouseleave', hideTechName);
+  svg.addEventListener('mouseenter', onTechMouseEnter(svg.dataset.techname));
+  svg.addEventListener('mouseleave', onTechMouseLeave);
 })
 
-function showTechName(name) {
+function onTechMouseEnter(name) {
   return () => {
     const t = tech_name_div;
     t.innerText = name;
     t.style.display = "block";
+    blurOtherTech(name);
   }
 }
 
-function hideTechName() {
+function onTechMouseLeave() {
   const t = tech_name_div;
   t.style.display = "none";
+  tech_stack_svgs.forEach(svg => blurElement(svg, false));
+}
+
+function blurOtherTech(name) {
+  [...tech_stack_svgs].filter(svg => svg.dataset.techname !== name).forEach(blurElement);
+}
+
+function blurElement(element, apply = true) {
+  apply && element.classList.add('blur-me');
+  !apply && element.classList.remove('blur-me');
 }
